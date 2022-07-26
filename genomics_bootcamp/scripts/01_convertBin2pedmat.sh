250aaa963ff8ad97c3a7cb50266a9605c90a01ca
Finn <tlobnow@googlemail.com>
2022-07-26 12:05:34 +0200

    updated

--- genomics_bootcamp/scripts/01_convertBin2pedmat.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-~/plink-1.9/plink \
---bfile /SAN/Ctyzzeri/genomics_bootcamp/resources/ADAPTmap_genotypeTOP_20160222_full \
---cow --nonfounders --allow-no-sex --recode \
---out ADAPTmap_TOP
