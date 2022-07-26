7b9669650ceae137043ef52dc237f6d165b8efc9
Finn <tlobnow@googlemail.com>
2022-07-26 16:14:25 +0200

    orga

--- Genome_Analysis/Scripts/09_filterVCF.sh
+++ Genome_Analysis/Scripts/09_filterVCF.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 
-VCF_IN=~/Documents/Github/Crypto/Genome_Analysis/products/vcf/12_samples.vcf.gz
-VCF_OUT=~/Documents/Github/Crypto/Genome_Analysis/products/vcfFiltered/12_samples_filtered.vcf.gz
-
 # set filters
 MAF=0.05
 MISS=0.75
 QUAL=30
 MIN_DEPTH=10
-MAX_DEPTH=50
+MAX_DEPTH=150
+
+
+VCF_IN=~/Documents/Github/Crypto/Genome_Analysis/products/vcf/15_samples.vcf.gz
+VCF_OUT=~/Documents/Github/Crypto/Genome_Analysis/products/vcfFiltered/15_samples_filtered.vcf.gz
+
+# perform the filtering with vcftools
+vcftools --gzvcf $VCF_IN \
+--remove-indels --maf $MAF --max-missing $MISS --minQ $QUAL \
+--min-meanDP $MIN_DEPTH --max-meanDP $MAX_DEPTH \
+--minDP $MIN_DEPTH --maxDP $MAX_DEPTH --recode --stdout | gzip -c > \
+$VCF_OUT
+
+VCF_IN=~/Documents/Github/Crypto/Genome_Analysis/products/vcf/12_samples.vcf.gz
+VCF_OUT=~/Documents/Github/Crypto/Genome_Analysis/products/vcfFiltered/12_samples_filtered.vcf.gz
 
 # perform the filtering with vcftools
 vcftools --gzvcf $VCF_IN \
